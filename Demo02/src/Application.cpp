94f1caa3ab56c945b2fb6f564ee46dbe3b56ef48
DESKTOP-5FNBKPC\hxb <1720502067@qq.com>
2020-07-10 16:31:14 +0800

    Class P23 Completed.

--- Demo02/src/Application.cpp
+++ Demo02/src/Application.cpp
@@ -54,10 +54,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			100.0f, 100.0f, 0.0f, 0.0f,
-			200.0f, 100.0f, 1.0f, 0.0f,
-			200.0f, 200.0f, 1.0f, 1.0f,
-			100.0f, 200.0f, 0.0f, 1.0f
+			-50.0f, -50.0f, 0.0f, 0.0f,
+			 50.0f, -50.0f, 1.0f, 0.0f,
+			 50.0f, 50.0f, 1.0f, 1.0f,
+			-50.0f, 50.0f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -79,14 +79,14 @@ int main(void)
 		IndexBuffer ib(indices, 6);
 
 		glm::mat4 proj = glm::ortho(0.f, 960.f, 0.f, 540.f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(-100, 0, 0));//Í¼ÏñÓÒ²àÒÆ¶¯100ÏñËØ
+		glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, 0));//Í¼ÏñÓÒ²àÒÆ¶¯100ÏñËØ
 		glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(200, 200, 0));
 		glm::mat4 mvp = proj * view * model;
 
 
 		Shader shader("res/shaders/Uniform.shader");
 		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		//shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/trump.png");
@@ -106,8 +106,9 @@ int main(void)
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init(glsl_version);
 
-		glm::vec3 translation(200, 200, 0);
-		
+		glm::vec3 translationA(200, 200, 0);
+		glm::vec3 translationB(400, 200, 0);
+
 		float r = 0.f;
 		float increment = .05f;
 		/* Loop until the user closes the window */
@@ -120,22 +121,30 @@ int main(void)
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 
-			glm::mat4 model = glm::translate(glm::mat4(1.f), translation);
-			glm::mat4 mvp = proj * view * model;
+			
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.f), translationA);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp);
-
-			renderer.Draw(va, ib, shader);
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.f), translationB);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
 
 			if (r > 1.0f || r < 0.f) increment *= -1;
 			r += increment;
 
 			{
-				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-				//ImGui::End();
 			}
 
 
