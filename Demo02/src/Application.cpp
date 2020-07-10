a0c38332de78ab963a2e1c983b2c7bbbb7d8d791
Maverick <1720502067@qq.com>
2020-07-10 17:38:55 +0800

    Class P25 Completed. Build TestMenu, have TestClearColor ready.

--- Demo02/src/Application.cpp
+++ Demo02/src/Application.cpp
@@ -22,6 +22,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -53,67 +55,28 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	{
-		float positions[] = {
-			-50.0f, -50.0f, 0.0f, 0.0f,
-			 50.0f, -50.0f, 1.0f, 0.0f,
-			 50.0f, 50.0f, 1.0f, 1.0f,
-			-50.0f, 50.0f, 0.0f, 1.0f
-		};
-
-		unsigned int indices[] = {
-			0, 1, 2,
-			2, 3, 0
-		};
-
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-		VertexArray va;
-		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-		VertexBufferLayout layout;
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		va.AddBuffer(vb, layout);
-
-		IndexBuffer ib(indices, 6);
-
-		glm::mat4 proj = glm::ortho(0.f, 960.f, 0.f, 540.f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(0, 0, 0));//Í¼ÏñÓÒ²àÒÆ¶¯100ÏñËØ
-		glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(200, 200, 0));
-		glm::mat4 mvp = proj * view * model;
-
-
-		Shader shader("res/shaders/Uniform.shader");
-		shader.Bind();
-		//shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", mvp);
-
-		Texture texture("res/textures/trump.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-		va.UnBind();
-		vb.UnBind();
-		ib.UnBind();
-		shader.UnBind();
 
 		Renderer renderer;
 
 		const char* glsl_version = "#version 330";
 		ImGui::CreateContext();
 		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsClassic();
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init(glsl_version);
 
-		glm::vec3 translationA(200, 200, 0);
-		glm::vec3 translationB(400, 200, 0);
+		test::Test* currentTest = nullptr;
+		test::TestMenu* testMenu = new test::TestMenu(currentTest);
+		currentTest = testMenu;
 
-		float r = 0.f;
-		float increment = .05f;
+		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
+			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 			/* Render here */
 			renderer.Clear();
 
@@ -121,32 +84,19 @@ int main(void)
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 
-			
+			if (currentTest)
 			{
-				glm::mat4 model = glm::translate(glm::mat4(1.f), translationA);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-				renderer.Draw(va, ib, shader);
+				currentTest->OnUpdate(0.f);
+				currentTest->OnRender();
+				ImGui::Begin("Test");
+				if (currentTest != testMenu && ImGui::Button("<-"))
+				{
+					delete currentTest;
+					currentTest = testMenu;
+				}
+				currentTest->OnImGuiRender();
+				ImGui::End();
 			}
-
-			{
-				glm::mat4 model = glm::translate(glm::mat4(1.f), translationB);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-				renderer.Draw(va, ib, shader);
-			}
-
-			if (r > 1.0f || r < 0.f) increment *= -1;
-			r += increment;
-
-			{
-				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			}
-
 
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -158,7 +108,12 @@ int main(void)
 			glfwPollEvents();
 		}
 
+		delete currentTest;
+
+		if (currentTest != testMenu)
+			delete testMenu;
 	}
+
 
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
---
+++ Demo02/src/tests/TestClearColor.cpp
@@ -0,0 +1,35 @@
+#include "TestClearColor.h"
+
+#include "Renderer.h"
+#include "imgui/imgui.h"
+
+namespace test {
+
+	TestClearColor::TestClearColor()
+		: m_ClearColor{ .2f,.3f,.8f,1.f }
+	{
+
+	}
+
+	TestClearColor::~TestClearColor()
+	{
+
+	}
+
+	void TestClearColor::OnUpdate(float deltaTime)
+	{
+
+	}
+
+	void TestClearColor::OnRender()
+	{
+		GLCall(glClearColor(m_ClearColor[0], m_ClearColor[1], m_ClearColor[2], m_ClearColor[3]));
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));
+	}
+
+	void TestClearColor::OnImGuiRender()
+	{
+		ImGui::ColorEdit4("Clear Color", m_ClearColor);
+	}
+
+}
